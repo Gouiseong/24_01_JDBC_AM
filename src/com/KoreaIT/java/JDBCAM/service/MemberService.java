17165c6c8da220d599c22b800e7cff6823df21ff
Gouiseong <rhdmltjd1324@gmail.com>
2024-01-11 14:45:13 +0900

    컨테이너 도입, SQL 에러메세지 console에 보이도록 변경

--- src/com/KoreaIT/java/JDBCAM/service/MemberService.java
+++ src/com/KoreaIT/java/JDBCAM/service/MemberService.java
@@ -1,15 +1,15 @@
 package com.KoreaIT.java.JDBCAM.service;
 
-import java.sql.Connection;
-
+import com.KoreaIT.java.JDBCAM.container.Container;
 import com.KoreaIT.java.JDBCAM.dao.MemberDao;
+import com.KoreaIT.java.JDBCAM.dto.Member;
 
 public class MemberService {
 
 	private MemberDao memberDao;
 
-	public MemberService(Connection conn) {
-		this.memberDao = new MemberDao(conn);
+	public MemberService() {
+		this.memberDao = Container.memberDao;
 	}
 
 	public boolean isLoginIdDup(String loginId) {
@@ -17,7 +17,11 @@ public class MemberService {
 	}
 
 	public int doJoin(String loginId, String loginPw, String name) {
-		return memberDao.doJoin(loginId,loginPw,name);
+		return memberDao.doJoin(loginId, loginPw, name);
+	}
+
+	public Member getMemberByLoginId(String loginId) {
+		return memberDao.getMemberByLoginId(loginId);
 	}
 
 }
